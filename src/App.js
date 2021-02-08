cfcf5d23f32b082ec443d53d075b38ef1ad8c0a2
tamasaaan <getchoo_oh_oh@yahoo.co.jp>
2021-02-08 15:58:14 +0900

    with react hook form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, {useCallback, useState, useEffect} from 'react'
 import create from 'zustand'
 import {useForm} from "react-hook-form";
+import { devtools } from 'zustand/middleware'
 
-const useDataStore = create((set) => ({
+const useDataStore = create(devtools((set) => ({
     data: {
         person: {
             name: "aaa",
@@ -10,36 +11,63 @@ const useDataStore = create((set) => ({
             age: ""
         }
     },
-    apply: (v) => set((state) => ({data: {person: v}}))
-}))
+    apply: (v) => set((state) => ({data: {person: v}})),
+    send: async (id, form, updateMode) => {
+        console.log(form)
+        const body = form
+        console.log("body", body)
+        const response = await fetch(`http://127.0.0.1:3106/users/${id}`, {
+            method: 'POST', // *GET, POST, PUT, DELETE, etc.
+            mode: 'cors', // no-cors, *cors, same-origin
+            body: {
+                person: JSON.stringify(body) // 本文のデータ型は "Content-Type" ヘッダーと一致する必要があります
+            } // 本文のデータ型は "Content-Type" ヘッダーと一致する必要があります
+        })
+        console.log(response.statusText)
+        // const data = await response.json()
+        set({ data: {person: form}})
+        updateMode("detail")
+        // (v) => set((state) => ({data: {person: v}})),
+    },
+    // load  pram id
+    load: async id => {
+        const response = await fetch(`http://127.0.0.1:3106/users/${id}`)
+        const data = await response.json()
+        set({ data: data})
+    }
+})))
+
+// const useDataStore = create(devtools(store))
 
 let renderCount = 0
 
 export default function App() {
 
-    const {data, apply} = useDataStore()
+    const {data, apply, load, send} = useDataStore()
     const {register, handleSubmit, reset, getValues} = useForm({
         defaultValues: data.person,
     });
+    const id = 1234
+
     useEffect(() => {
-        const id = 1234
-        fetch(`http://127.0.0.1:3105/users/${id}`)
-            .then(response => response.json())
-            .then(res => {
-                console.log("api", res)
-                apply(res.person)
-                reset(res.person)
-            });
-    }, [apply])
+        // storeが変わったら毎回reset subscribe
+        // 国籍など
+        return useDataStore.subscribe(reset, state => state.data.person)
+    }, [reset])
+
+    useEffect(() => {
+        // 適当なgetapiを使用
+        load(id)
+    }, [load])
 
     const [mode, updateMode] = useState("detail")
 
     const onSubmit = useCallback((form) => {
         console.log("form", form)
-        apply(form)
-        updateMode("detail")
-        reset(form)
-    }, [apply, updateMode, reset])
+        // apply(form)
+        send(id, form, updateMode)
+        // reset
+    }, [apply, updateMode, send])
 
     const handleReset = useCallback(() => {
         reset(data.person)
@@ -52,7 +80,7 @@ export default function App() {
     return (
 
         <div style={{backgroundColor: "#fff"}}>
-            {renderCount}
+            renderCount:{renderCount}
 
             {
                 mode === "edit" ?
